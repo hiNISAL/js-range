fcfbda1036773fe717a77069d4e0248c36d976be
NISAL <535964903@qq.com>
2021-11-12 11:22:25 +0800

    chore: rewrite hole project.

---
+++ dist/index.js
@@ -0,0 +1,45 @@
+/*!
+ * jrange 1.1.1 (https://github.com/hiNISAL/jrange)
+ * API https://github.com/hiNISAL/jrange/blob/master/doc/api.md
+ * Copyright 2017-2021 hiNISAL. All Rights Reserved
+ * Licensed under MIT (https://github.com/hiNISAL/jrange/blob/master/LICENSE)
+ */
+
+'use strict';
+
+function index (start, end, step) {
+    if (step === void 0) { step = 1; }
+    if (arguments.length === 0) {
+        return [];
+    }
+    if (arguments.length === 1) {
+        end = start;
+        start = 0;
+    }
+    // 如果步长是0
+    // 这种情况错误 返回空
+    if (step === 0) {
+        return [];
+    }
+    // 起始和结束相等 则返回空
+    if (start === end) {
+        return [];
+    }
+    // 如果起始比结束大 并且步长为正数
+    // 这种情况是错误的 因为会进入无限循环
+    if (start > end && step >= 0) {
+        return [];
+    }
+    // 如果起始比结束小 并且步长为负数
+    // 这种情况是错误的 因为会进入无限循环
+    if (start < end && step <= 0) {
+        return [];
+    }
+    var res = [];
+    for (var i = start; i < end; i += step) {
+        res.push(i);
+    }
+    return res;
+}
+
+module.exports = index;
---
+++ src/index.ts
@@ -0,0 +1,41 @@
+export default function (start?: number, end?: number, step: number = 1): number[] {
+  if (arguments.length === 0) {
+    return [];
+  }
+
+  if (arguments.length === 1) {
+    end = start;
+    start = 0;
+  }
+
+  // 如果步长是0
+  // 这种情况错误 返回空
+  if (step === 0) {
+    return [];
+  }
+
+  // 起始和结束相等 则返回空
+  if (start === end) {
+    return [];
+  }
+
+  // 如果起始比结束大 并且步长为正数
+  // 这种情况是错误的 因为会进入无限循环
+  if (start > end && step >= 0) {
+    return [];
+  }
+
+  // 如果起始比结束小 并且步长为负数
+  // 这种情况是错误的 因为会进入无限循环
+  if (start < end && step <= 0) {
+    return [];
+  }
+
+  const res = [];
+
+  for (let i = start; i < end; i += step) {
+    res.push(i);
+  }
+
+  return res;
+}
